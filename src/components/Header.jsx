e90faa91a2ce6fd63f4ef4f1b7c41536e7f6abc8
윤진 <151013932+maruru301@users.noreply.github.com>
2025-05-30 22:04:13 +0900

    feat: 헤더 검색창 구현 #3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+import searchIcon from '../assets/search-icon.svg';
+import textInputAssistantImg from '../assets/text-input-assistant.png';
+
 const Header = () => {
     return (
         <header>
@@ -5,6 +8,30 @@ const Header = () => {
                 <a href="#" className="logo-padding">
                     <img src="/yt-logo.svg" alt="로고"></img>
                 </a>
+
+                <div className="flex items-center basis-[732px] shrink grow-0">
+                    <div className="search-box flex items-center flex-1 ml-10 h-10">
+                        <div className="input-box">
+                            <form action="/" className="search-form flex flex-1 h-6">
+                                <input type="text" className="search-input w-full" placeholder="검색" />
+                            </form>
+
+                            <button className="text-input-assistant-btn p-1">
+                                <img src={textInputAssistantImg} alt="텍스트 입력 보조 키보드" />
+                            </button>
+                        </div>
+
+                        <button className="search-btn" type="submit" title="검색">
+                            <div className="flex w-6 h-6">
+                                <img className="w-full h-full" src={searchIcon} alt="돋보기 아이콘" />
+                            </div>
+                        </button>
+                    </div>
+
+                    <div className="border">마이크 버튼</div>
+                </div>
+
+                <div>사용자탭</div>
             </div>
         </header>
     );
