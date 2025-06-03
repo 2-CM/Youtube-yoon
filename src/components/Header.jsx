955c9c68e20f2ac8c799fae04f58c987ec071175
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-04 00:48:40 +0900

    style: 테마 변경 버튼과 사용자 프로필 버튼 구현 #3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
+import moonIcon from '../assets/moon-icon.svg';
 import searchIcon from '../assets/search-icon.svg';
+import sunIcon from '../assets/sun-icon.svg';
 import textInputAssistantImg from '../assets/text-input-assistant.png';
+import userProfile from '../assets/user-profile.jpg';
 import voiceSearchIcon from '../assets/voice-search-icon.svg';
 
 const Header = () => {
@@ -47,8 +50,21 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div>
-                    <div></div>
+                <div className="flex items-center">
+                    <button className="flex items-center mr-2 p-2 rounded-3xl bg-gray-100 hover:bg-[rgba(0,0,0,0.15)]">
+                        <img className="w-full h-full" src={moonIcon} alt="" />
+                    </button>
+
+                    <div className="flex px-1">
+                        <button className="mx-2 w-8 h-8 rounded-full overflow-hidden">
+                            <img
+                                className="w-full h-full object-cover"
+                                draggable="false"
+                                src={userProfile}
+                                alt="사용자 프로필 이미지"
+                            />
+                        </button>
+                    </div>
                 </div>
             </div>
         </header>
