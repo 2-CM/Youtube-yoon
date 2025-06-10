4e50a0d31d32c3207a5015189d16d7a10ea6a3ec
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-10 23:20:43 +0900

    feat: SideBar 레이아웃 구현 #5

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,20 @@
-const Sidebar = () => {
+import { CircleUserRound, House } from 'lucide-react';
+
+const SideBar = () => {
     return (
-        <div>
-            <h1 className="h1">Sidebar</h1>
+        <div className="w-[72px]">
+            <div className="mt-1 flex flex-col items-center w-full">
+                <a className="sidebar-item active:" href="/">
+                    <House />
+                    <span className="sidebar-text">홈</span>
+                </a>
+                <a className="sidebar-item" href="#">
+                    <CircleUserRound />
+                    <span className="sidebar-text">내 페이지</span>
+                </a>
+            </div>
         </div>
     );
 };
 
-export default Sidebar;
+export default SideBar;
