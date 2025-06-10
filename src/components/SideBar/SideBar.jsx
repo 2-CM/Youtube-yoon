5581ba50b944aff930122959c33d9905ff267d17
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-10 23:54:23 +0900

    style: SideBar 고정 #5

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,7 +4,7 @@ import SideBarItem from './SideBarItem';
 
 const SideBar = () => {
     return (
-        <div className="w-[72px]">
+        <div className="fixed left-0 bottom-0 top-14 w-[72px] px-1">
             <div className="mt-1 flex flex-col items-center w-full">
                 <SideBarItem icon={House} href="/" alt="홈 버튼" text="홈" />
                 <SideBarItem icon={CircleUserRound} href="#" alt="마이페이지 버튼" text="내 페이지" />
