179961df13b43c2c1074ab1dbac6246da74f2377
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-11 00:37:06 +0900

    feat: 현재 URL 경로와 버튼 링크를 비교해 활성화 상태 표시 기능 추가 #5

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -7,7 +7,7 @@ const SideBar = () => {
         <div className="fixed left-0 bottom-0 top-14 w-[72px] px-1">
             <div className="mt-1 flex flex-col items-center w-full">
                 <SideBarItem icon={House} href="/" alt="홈 버튼" text="홈" />
-                <SideBarItem icon={CircleUserRound} href="#" alt="마이페이지 버튼" text="내 페이지" />
+                <SideBarItem icon={CircleUserRound} href="/mypage" alt="마이페이지 버튼" text="내 페이지" />
             </div>
         </div>
     );
--- src/components/SideBar/SideBarItem.jsx
+++ src/components/SideBar/SideBarItem.jsx
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom';
+
 const SideBarItem = ({ icon: Icon, href, alt, text }) => {
+    const location = useLocation();
+    // 현재 URL 경로(location.pathname)와 버튼 링크(href)를 비교
+    const isActive = location.pathname === href;
+
     return (
         <div className="sidebar-item-container">
-            <a className="sidebar-item" href={href}>
+            <a className={`sidebar-item ${isActive ? 'text-red-500' : ''}`} href={href}>
                 <Icon strokeWidth={1.5} alt={alt} />
                 <span className="sidebar-text">{text}</span>
             </a>
