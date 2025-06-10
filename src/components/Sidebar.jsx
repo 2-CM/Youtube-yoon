40cb60581f27df8ffed802932dce246bc7c171ff
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-10 23:31:34 +0900

    feat: hover 또는 active 시 사이드바 메뉴의 배경색 설정 #5

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,18 @@ const SideBar = () => {
     return (
         <div className="w-[72px]">
             <div className="mt-1 flex flex-col items-center w-full">
-                <a className="sidebar-item active:" href="/">
-                    <House />
-                    <span className="sidebar-text">홈</span>
-                </a>
-                <a className="sidebar-item" href="#">
-                    <CircleUserRound />
-                    <span className="sidebar-text">내 페이지</span>
-                </a>
+                <div className="sidebar-item-container">
+                    <a className="sidebar-item" href="/">
+                        <House />
+                        <span className="sidebar-text">홈</span>
+                    </a>
+                </div>
+                <div className="sidebar-item-container">
+                    <a className="sidebar-item" href="#">
+                        <CircleUserRound />
+                        <span className="sidebar-text">내 페이지</span>
+                    </a>
+                </div>
             </div>
         </div>
     );
