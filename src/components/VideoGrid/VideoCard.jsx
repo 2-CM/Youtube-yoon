4ee452c104e4ca025f1ed7cbd978f7bde9762cc5
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-11 22:21:34 +0900

    feat: VideoCard 레이아웃 구현 #7

--- src/components/VideoGrid/VideoCard.jsx
+++ src/components/VideoGrid/VideoCard.jsx
@@ -1,5 +1,23 @@
+import VideoThumnail from '../../assets/video_thumbnail.avif';
+
 const VideoCard = () => {
-    return <div className="w-80 h-48">카드</div>;
+    return (
+        <div className="h-full bg-gray-200">
+            <div className="">
+                <div className="h-full overflow-hidden rounded-xl">
+                    <img className="w-full h-full object-cover" src={VideoThumnail} alt="영상 썸네일 사진" />
+                </div>
+            </div>
+            <div className="flex">
+                <div className="mt-3 mr-3">채널사진</div>
+                <div className="pr-6">
+                    <div className="mt-3 mb-1">영상제목</div>
+                    <div>채널명</div>
+                    <div>조회수</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default VideoCard;
--- src/components/VideoGrid/VideoGrid.jsx
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -2,8 +2,15 @@ import VideoCard from './VideoCard';
 
 const VideoGrid = () => {
     return (
-        <div className="bg-blue-600 mt-14 ml-[72px]">
-            <div className="grid">
+        <div className="mt-14 ml-[72px] w-full">
+            <div className="w-full grid grid-cols-3 gap-x-4 gap-y-8 px-4 pt-6 pb-8">
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
                 <VideoCard />
                 <VideoCard />
                 <VideoCard />
