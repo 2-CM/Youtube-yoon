dd489caf89a211e3144ff52bec0065a6af701b21
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-12 21:36:13 +0900

    feat: 채널 프로필 사진과 영상 제목 레이아웃 구현 #7

--- src/components/VideoGrid/VideoCard.jsx
+++ src/components/VideoGrid/VideoCard.jsx
@@ -1,8 +1,9 @@
+import UserProfile from '../../assets/user-profile.jpg';
 import VideoThumnail from '../../assets/video_thumbnail.avif';
 
 const VideoCard = () => {
     return (
-        <div className="h-full bg-gray-200 relative">
+        <div className="bg-gray-200 relative">
             <div className="">
                 <div className="h-full overflow-hidden rounded-xl">
                     <img
@@ -14,9 +15,16 @@ const VideoCard = () => {
                 </div>
             </div>
             <div className="flex">
-                <div className="mt-3 mr-3">채널사진</div>
+                <div className="mt-3 mr-3 overflow-hidden rounded-full w-9 h-9">
+                    <a href="/">
+                        <img className="w-full h-full object-cover" src={UserProfile} alt="채널 프로필 사진" />
+                    </a>
+                </div>
                 <div className="pr-6">
-                    <div className="mt-3 mb-1">영상제목</div>
+                    <div className="mt-3 mb-1 text-base font-medium line-clamp-2">
+                        흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금 흘러가는대로
+                        살고싶은 브금 흘러가는대로 살고싶은 브금
+                    </div>
                     <div>채널명</div>
                     <div>조회수</div>
                 </div>
