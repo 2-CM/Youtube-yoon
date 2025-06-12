dc70414f483bc7e2dad163b50b01a83beb67d18c
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-12 22:51:10 +0900

    feat: VideoCard에 메타 정보(채널명, 조회수, 업로드 시점) 표시 #7

--- src/components/VideoGrid/VideoCard.jsx
+++ src/components/VideoGrid/VideoCard.jsx
@@ -3,35 +3,45 @@ import VideoThumnail from '../../assets/video_thumbnail.avif';
 
 const VideoCard = () => {
     return (
-        <div className="bg-gray-200 relative">
-            <div className="">
-                <div className="h-full overflow-hidden rounded-xl">
-                    <img
-                        className="w-full h-full object-cover "
-                        src={VideoThumnail}
-                        draggable="false"
-                        alt="영상 썸네일 사진"
-                    />
-                </div>
-            </div>
-            <div className="flex">
-                <div className="mt-3 mr-3 overflow-hidden rounded-full w-9 h-9 flex-shrink-0">
-                    <a href="/">
-                        <img className="w-full h-full object-cover" src={UserProfile} alt="채널 프로필 사진" />
-                    </a>
-                </div>
-                <div className="pr-6">
-                    <div className="mt-3 mb-1 text-base font-medium line-clamp-2">
-                        흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금 흘러가는대로
-                        살고싶은 브금 흘러가는대로 살고싶은 브금
+        <a href="/video">
+            <div className="relative">
+                <div className="">
+                    <div className="h-full overflow-hidden rounded-xl">
+                        <img
+                            className="w-full h-full object-cover "
+                            src={VideoThumnail}
+                            draggable="false"
+                            alt="영상 썸네일 사진"
+                        />
                     </div>
-                    <div>채널명</div>
-                    <div>조회수</div>
+                </div>
+                <div className="flex">
+                    <div className="mt-3 mr-3 overflow-hidden rounded-full w-9 h-9 flex-shrink-0">
+                        <a href="/user">
+                            <img
+                                className="w-full h-full object-cover"
+                                src={UserProfile}
+                                draggable="false"
+                                alt="채널 프로필 사진"
+                            />
+                        </a>
+                    </div>
+                    <div className="pr-6">
+                        <a href="/video">
+                            <div className="mt-3 mb-1 text-base font-medium line-clamp-2">
+                                흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금
+                                흘러가는대로 살고싶은 브금 흘러가는대로 살고싶은 브금
+                            </div>
+                        </a>
+                        <div className="text-sm  font-normal text-[#606060]">
+                            <div className="hover:text-[#0f0f0f] line-clamp-2 break-all">동숲거주자</div>
+                            <span>조회수 10만회</span>
+                            <span className="before:content-['•'] before:mx-1">6개월 전</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <a className="absolute top-0 w-full h-full" href="/"></a>
-        </div>
+        </a>
     );
 };
 
