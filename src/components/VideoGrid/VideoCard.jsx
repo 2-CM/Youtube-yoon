10ffaae474f4b5c6f1d94281e40b92248704ff9e
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-13 02:03:06 +0900

    feat: 카드 클릭 이벤트 분리 및 카드 영역 active 시 배경 효과 추가 #7
    - handleCardClick: 카드 클릭 시 영상 상세 페이지로 이동
      (최상위 태그를 a → div로 변경함에 따라 사용. 중첩된 a 태그 방지 목적)
    - VideoCard 전체 영역 클릭 시 배경 어두워지는 효과 추가 (bg)
    - border 효과는 추후 적용 예정으로 주석 처리

--- src/components/VideoGrid/VideoCard.jsx
+++ src/components/VideoGrid/VideoCard.jsx
@@ -2,46 +2,58 @@ import UserProfile from '../../assets/user-profile.jpg';
 import VideoThumnail from '../../assets/video_thumbnail.avif';
 
 const VideoCard = ({ videoId, username, title, views, uploadedAt }) => {
+    const handleCardClick = () => {
+        window.location.href = `/video/${videoId}`;
+    };
+
     return (
-        <a href={`/video/${videoId}`}>
-            <div className="relative">
-                <div className="">
-                    <div className="h-full overflow-hidden rounded-xl">
+        <div onClick={handleCardClick} className="relative cursor-pointer group/click">
+            {/* 썸네일 */}
+            <div>
+                <div className="h-full overflow-hidden rounded-xl">
+                    <img
+                        className="w-full h-full object-cover "
+                        src={VideoThumnail}
+                        draggable="false"
+                        alt="영상 썸네일 사진"
+                    />
+                </div>
+            </div>
+
+            {/* 콘텐츠 하단 영역 */}
+            <div className="relative flex">
+                {/* 프로필 이미지 (유저 페이지 이동) */}
+                <div className="mt-3 mr-3 overflow-hidden rounded-full w-9 h-9 flex-shrink-0">
+                    <a href={`/@${username}`}>
                         <img
-                            className="w-full h-full object-cover "
-                            src={VideoThumnail}
+                            className="w-full h-full object-cover"
+                            src={UserProfile}
                             draggable="false"
-                            alt="영상 썸네일 사진"
+                            alt="채널 프로필 사진"
                         />
-                    </div>
+                    </a>
                 </div>
 
-                <div className="relative flex">
-                    <div className="mt-3 mr-3 overflow-hidden rounded-full w-9 h-9 flex-shrink-0">
-                        <a href={`/@${username}`}>
-                            <img
-                                className="w-full h-full object-cover"
-                                src={UserProfile}
-                                draggable="false"
-                                alt="채널 프로필 사진"
-                            />
-                        </a>
-                    </div>
+                {/* 텍스트 영역 */}
+                <div className="pr-6">
+                    <div className="mt-3 mb-1 text-base font-medium line-clamp-2">{title}</div>
 
-                    <div className="pr-6">
-                        <a href={`/video/${videoId}`}>
-                            <div className="mt-3 mb-1 text-base font-medium line-clamp-2">{title}</div>
-                        </a>
-
-                        <div className="group text-sm font-normal text-[#606060]">
+                    <div className="text-sm font-normal text-[#606060]">
+                        <a className="group" href={`/@${username}`}>
                             <div className="hover:text-[#0f0f0f] line-clamp-2 break-all">{username}</div>
-                            <div className="tooltip left-13 top-1">{username}</div>
-                            <span>{`${views} • ${uploadedAt}`}</span>
-                        </div>
+                            <div className="tooltip left-13 top-1 ">{username}</div>
+                        </a>
+                        <span>{`${views} • ${uploadedAt}`}</span>
                     </div>
                 </div>
             </div>
-        </a>
+
+            {/* 클릭 효과 */}
+            <div className="absolute inset-0 -m-1 pointer-events-none">
+                <div className="videocard-click-bg"></div>
+                {/* <div className="videocard-click-border"></div> */}
+            </div>
+        </div>
     );
 };
 
