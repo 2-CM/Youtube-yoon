4fbe68f4da2924d379a2d2b4d837a244a53f8948
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-19 22:47:34 +0900

    feat: VideoDetail에 임시 영상 iframe 추가 및 레이아웃 잡기 #9

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -5,7 +5,15 @@ const VideoDetail = () => {
         <>
             <div className="flex mt-14">
                 <div className="pt-6 ml-6 pr-6 flex-1">
-                    <div>영상</div>
+                    <div className="aspect-video bg-gray-200 rounded-lg overflow-hidden">
+                        <iframe
+                            className="w-full h-full"
+                            src={`https://www.youtube.com/embed/0D-dHRtyiyE`}
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
                     <div>영상 정보</div>
                 </div>
 
