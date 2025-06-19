4d67c2d3b811ba4a2b463903428531bec1981093
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-19 22:23:05 +0900

    feat: VideoDetail 페이지 레이아웃 기본 구조 구현 #9

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,7 +1,18 @@
 import { useParams } from 'react-router-dom';
 
 const VideoDetail = () => {
-    return <></>;
+    return (
+        <>
+            <div className="flex mt-14">
+                <div className="pt-6 ml-6 pr-6 flex-1">
+                    <div>영상</div>
+                    <div>영상 정보</div>
+                </div>
+
+                <div className="pt-6 pr-6 w-[402px] min-w-[300px]">추천 영상</div>
+            </div>
+        </>
+    );
 };
 
 export default VideoDetail;
