b95d160efd119d9a362103dec6b4e3f459b9cdaf
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-25 22:39:48 +0900

    feat: VideoInfo에 영상 채널 이름(username) 툴팁 구현 #9

--- src/components/VideoDetail/VideoInfo.jsx
+++ src/components/VideoDetail/VideoInfo.jsx
@@ -5,12 +5,15 @@ const VideoInfo = ({ title, username, description }) => {
         <div className="mt-3 mb-6">
             <div className="video-title text-xl font-bold">{title}</div>
 
-            <div className="channel-info flex items-center w-fit mr-3 mt-3">
+            <div className="channel-info relative flex items-center w-fit mr-3 mt-3">
                 <div className="rounded-full overflow-hidden w-10 h-10 mr-4 cursor-pointer">
                     <img src={UserProfile} draggable="false" alt={`${username} 채널 사진`} />
                 </div>
 
-                <div className="flex items-center font-medium text-base cursor-pointer">{username}</div>
+                <div className="group">
+                    <div className="flex items-center font-medium text-base cursor-pointer">{username}</div>
+                    <div className="tooltip -top-16">{username}</div>
+                </div>
             </div>
 
             <div className="video-description mt-3 bg-[rgba(0,0,0,0.05)] rounded-xl text-sm font-normal cursor-pointer">
