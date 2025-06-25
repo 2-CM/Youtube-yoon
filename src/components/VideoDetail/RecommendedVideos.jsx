800d5fa113c68b5b31a80701eca7a7ef7fcae29f
윤진 <151013932+maruru301@users.noreply.github.com>
2025-06-25 20:15:18 +0900

    style: 추천 영상 개수 수정 #9

--- src/components/VideoDetail/RecommendedVideos.jsx
+++ src/components/VideoDetail/RecommendedVideos.jsx
@@ -3,7 +3,7 @@ import VideoCard from '../VideoGrid/VideoCard';
 const RecommendedVideos = ({ videos, onCardClick }) => {
     return (
         <div className="recommended-videos pt-6 pb-6 mr-6 w-[402px] min-w-[300px]">
-            {videos.slice(0, 10).map((video) => (
+            {videos.map((video) => (
                 <VideoCard
                     key={video.videoId}
                     {...video}
