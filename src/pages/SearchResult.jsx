ff93fe7ad6b4d2128bbe086b9d769e0cfc57342e
윤진 <151013932+maruru301@users.noreply.github.com>
2025-07-30 22:43:17 +0900

    feat: 검색 결과 페이지(SearchResult) 추가 및 라우트 등록 #19

---
+++ src/pages/SearchResult.jsx
@@ -0,0 +1,13 @@
+import SideBar from '../components/SideBar/SideBar';
+import VideoGrid from '../components/VideoGrid/VideoGrid';
+
+const SearchResult = () => {
+    return (
+        <div className="flex">
+            <SideBar />
+            <VideoGrid />
+        </div>
+    );
+};
+
+export default SearchResult;
