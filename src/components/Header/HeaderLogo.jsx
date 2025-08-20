8ede3013127d08800d8687595abfe08c4b93ddfb
윤진 <151013932+maruru301@users.noreply.github.com>
2025-08-21 01:03:16 +0900

    fix: HeaderLogo에서 img 태그 className으로 변경 #21

--- src/components/Header/HeaderLogo.jsx
+++ src/components/Header/HeaderLogo.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 const HeaderLogo = () => {
     return (
         <Link to="/" className="logo-padding flex-shrink-0">
-            <img src="/yt-logo-on-light.svg" class="block dark:hidden" draggable="false" alt="라이트 모드 로고"></img>
-            <img src="/yt-logo-on-dark.svg" class="hidden dark:block" draggable="false" alt="다크 모드 로고"></img>
+            <img
+                src="/yt-logo-on-light.svg"
+                className="block dark:hidden"
+                draggable="false"
+                alt="라이트 모드 로고"
+            ></img>
+            <img src="/yt-logo-on-dark.svg" className="hidden dark:block" draggable="false" alt="다크 모드 로고"></img>
         </Link>
     );
 };
